0e76669d0829340b6a1900c294fe09a54caca4c5
LESTOUN Aya <eyalestoun13@gmail.com>
2025-04-25 23:35:36 +0100

    first commit

---
+++ src/test/java/net/aya/student_app/StudentAppApplicationTests.java
@@ -0,0 +1,14 @@
+package net.aya.student_app;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
